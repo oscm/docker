d7518f49795116ce9f5f3c578bd87d4c859a4323
Neo Chen <netkiller@msn.com>
2017-06-14 10:19:50 +0800

    modified:   setenv.sh

--- centos/tomcat/setenv.sh
+++ centos/tomcat/setenv.sh
@@ -1,4 +1,1 @@
-export JAVA_HOME=/usr/java/default
 export JAVA_OPTS="-server -Xms2048m -Xmx4096m -Djava.io.tmpdir=/tmp -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=UTF8  -Duser.timezone=GMT+08"
-export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib:.
-export PATH=$PATH:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:
