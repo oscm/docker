94cfc7c9d19abe9fb2883251ddaa7213742d2e73
Neo Chen <Neo Chen netkiller@msn.com>
2017-06-15 09:32:05 +0800

    update

--- centos/tomcat/setenv.sh
+++ centos/tomcat/setenv.sh
@@ -1,1 +1,2 @@
-export JAVA_OPTS="-server -Xms2048m -Xmx4096m -Djava.io.tmpdir=/tmp -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=UTF8  -Duser.timezone=GMT+08"
+export JAVA_OPTS="-server -Xms2048m -Xmx4096m  -Djava.security.egd=file:/dev/./urandom
+# -Dfile.encoding=UTF8  -Duser.timezone=GMT+08"
