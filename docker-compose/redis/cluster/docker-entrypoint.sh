75d75c3cb9d3bb1494e1052e2a0756e1cdd62827
Neo Chan <netkiller@msn.com>
2019-04-16 14:51:40 +0800

    docker

---
+++ docker-compose/redis/cluster/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+echo never | tee /sys/kernel/mm/transparent_hugepage/enabled
+echo never | tee /sys/kernel/mm/transparent_hugepage/defrag
+
+redis-server /etc/redis/redis.conf
