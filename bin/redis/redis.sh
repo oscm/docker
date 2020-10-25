36a2adde5e315804beb4f5ae582cd5c3ca4eaa92
Neo Chan <netkiller@msn.com>
2020-10-25 18:53:04 +0800

    redis

--- bin/redis/redis.sh
+++ bin/redis/redis.sh
@@ -1,3 +1,3 @@
 docker pull redis
 
-docker run --name redis -v /docker/host/dir:/data -p 6379:6379 -d redis redis-server --appendonly yes
+docker run --name redis -v /var/lib/redis:/data -p 6379:6379 -d redis redis-server --appendonly yes
