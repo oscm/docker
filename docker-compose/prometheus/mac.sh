568bc66885d8dbcffe1476c16ac036aff3ad4b05
Neo Chan <netkiller@msn.com>
2020-10-29 23:20:58 +0800

    monitor

--- docker-compose/prometheus/mac.sh
+++ docker-compose/prometheus/mac.sh
@@ -1,7 +1,7 @@
 # sudo useradd -rs /bin/false prometheus
 # sudo useradd -rs /bin/false node_exporter
 
-sudo mkdir -p /var/lib/{prometheus,grafana} /var/log/grafana
-sudo chown neo:staff /var/lib/{prometheus,grafana}
-sudo chmod 777 /var/lib/{prometheus,grafana} /var/log/grafana
+mkdir -p /var/lib/{prometheus,grafana,alertmanager} /var/log/grafana
+chown neo:staff /var/lib/{prometheus,grafana,alertmanager}
+chmod 777 /var/lib/{prometheus,grafana,alertmanager} /var/log/grafana
 docker-compose up
